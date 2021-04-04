61fb22c38cbc8fd0c15b1f33555a5d685741734f
Kamalakar Kududhula <kududhula.k@gmail.com>
2021-04-04 11:55:53 -0500

    added kafka producer

--- log-producer/main.py
+++ log-producer/main.py
@@ -3,6 +3,37 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+'''
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+import re
+
+f = open("/Users/a569514/Downloads/others/foobar.txt", "r")
+#f = open("foobar.txt", "r")
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+for x in f:
+  out = re.findall('([^ ]) - - \[(.)\] "(.)" ([0-9]) ([0-9]) "(.)" "(.*)"', x)
+  ip, timestamp = out[0][0:2]
+  if(ip==None or len(ip) < 5):
+    print("===========> EMPTY IP")
+  if(timestamp==None or len(timestamp) < 4):
+    print("===========> EMPTY timestamp")
+  logMsg={}
+  #print(ip + " --- " +timestamp)
+  logMsg['ip'] = ip
+  logMsg['timestamp'] = timestamp
+  producer.send('logging_topic_2', value=logMsg)
+  #sleep(0.5)
+
+'''
+
+
 
 
 def print_hi(name):
