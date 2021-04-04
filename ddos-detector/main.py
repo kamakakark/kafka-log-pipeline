08b2e5ae5dfd4779796b915dc77fc8a1b72c8aae
Kamalakar Kududhula <kududhula.k@gmail.com>
2021-04-04 14:34:30 -0500

    kafka log consumer init

--- ddos-detector/main.py
+++ ddos-detector/main.py
@@ -3,7 +3,44 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+'''
+from kafka import KafkaConsumer
+from json import loads
+from time import sleep
 
+
+
+consumer = KafkaConsumer(
+    'logging_topic_2',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group-id-6',
+    value_deserializer=lambda x: loads(x.decode('utf-8'))
+)
+
+dict_cnt= {}
+THRESHOLD_VALUE=3
+writeFile = open("foobar2.txt", "a")
+
+for event in consumer:
+    event_data = event.value
+    # Do whatever you want
+    ip = event_data['ip']
+
+    if(ip in dict_cnt.keys()):
+       dict_cnt[ip] += 1
+
+    else:
+       dict_cnt[ip] = 1
+
+    if(dict_cnt[ip] == THRESHOLD_VALUE):
+      writeFile.write(ip + "\n")
+
+      print(ip + "  ----->  " + str(dict_cnt[ip]))
+
+f.close()
+'''
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
