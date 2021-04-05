3afa6d7acb6a7975ed80310e643a10e1a303da2e
Kamal.Kududhula <Kamal.Kududhula@target.com>
2021-04-04 23:48:45 -0500

    refactored

--- log-producer/log-producer-main.py
+++ log-producer/log-producer-main.py
@@ -1,33 +1,36 @@
 
-from json import dumps
-from kafka import KafkaProducer
-import sys
 import os
+import sys
+from json import dumps
 
-TOPIC_NAME='logging_topic_3'
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+TOPIC_NAME='logging_topic_4'
+
+def get_kafka_producer(bootstrap_server):
+    try:
+        return KafkaProducer(
+            bootstrap_servers=[bootstrap_server],
+            value_serializer=lambda x: dumps(x).encode('utf-8')
+        )
+    except KafkaError as ex:
+        print("Exception during kafka producer connection - {}".format(ex))
 
 def consume_logs(bootstrap_server, input_file):
 
-    producer = KafkaProducer(
-        bootstrap_servers=[bootstrap_server],
-        value_serializer=lambda x: dumps(x).encode('utf-8')
-    )
-    print input_file
+    producer = get_kafka_producer(bootstrap_server)
     if os.path.isfile(input_file):
         current_file = open(input_file, "r")
         for line in current_file:
             if (line != None):
                 producer.send(TOPIC_NAME, value=line)
     else:
-        print "provided path does not exists : "+input_file
+        print("Provided path does not exists : {}".format(input_file))
 
 def main(arguments):
-
-    #print 'Arguments %s' %arguments
-    print '-----------------------------------------------------------------------------'
-    print 'Function Name    :  main(arguments)'
-    print 'bootstrap-server  :  %s' %arguments[1]
-    print 'Input-file  :  %s' %arguments[2]
+    print("bootstrap-server  - {}".format(arguments[1]))
+    print("Input-file - {}".format(arguments[2]))
     consume_logs(arguments[1], arguments[2])
 
 if __name__ == '__main__':
